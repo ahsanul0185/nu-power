a7127343141498c7428a55fbcfa4d0c383a7dae0
ahsanul0185 <ahsanul0185@gmail.com>
2025-02-26 22:01:49 +0600

    responsive chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -7,12 +7,12 @@ const ChatBot = () => {
   const [showChatBox, setShowChatBox] = useState(false);
 
   return (
-    <div className="fixed bottom-24 right-6 bg-blue- 400 flex items-end">
+    <div className="fixed bottom-24 right-6 w-full pl-12 sm:pl-0 sm:w-fit flex flex-col bg-v iolet-500 sm:flex-row items-end gap-3 sm:gap-0">
 
-      <motion.div
-        initial={{ width: 0, height: 0, opacity : 0 }}
-        animate={{width : showChatBox ? 400 : 0, height : showChatBox ? 500 : 0, opacity : showChatBox ? 1 : 0}}
-        className="overflow-clip"
+      <div
+        // initial={{ width: 0, height: 0, opacity : 0 }}
+        // animate={{width : showChatBox ? 400 : 0, height : showChatBox ? 500 : 0, opacity : showChatBox ? 1 : 0}}
+        className={`overflow-clip duration-300 ${showChatBox ? "w-full sm:w-[400px] h-[500px] opacity-100" : " opacity-0 w-0 h-0"}`}
       >
         <iframe
           src="https://app.wonderchat.io/chatbot/cm7lnwv9a09educqvsy73i4qk"
@@ -20,7 +20,7 @@ const ChatBot = () => {
           className={`w-full h-full `}
           id="wonderchat-iframe"
         ></iframe>
-      </motion.div>
+      </div>
 
       <button
         className="size-12 cursor-pointer"
